90a14c6102fda73f1d1844da101e32149ad03219
BuildTools <unconfigured@null.spigotmc.org>
2024-01-21 15:25:44 +0100

    Add official method for create head

--- src/main/java/net/arcaniax/gopaint/utils/Items.java
+++ src/main/java/net/arcaniax/gopaint/utils/Items.java
@@ -20,12 +20,17 @@ package net.arcaniax.gopaint.utils;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 
 import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -63,7 +68,20 @@ public class Items {
             item = new ItemStack(Material.getMaterial("SKULL_ITEM"));
             item.setDurability((short) 3);
         }
+
+        // https://blog.jeff-media.com/creating-custom-heads-in-spigot-1-18-1/
+        PlayerProfile pProfile = getProfile(data);
+
+        item = new ItemStack(Material.PLAYER_HEAD );
+        SkullMeta skullMeta = (SkullMeta) item.getItemMeta();
+
+        ((SkullMeta) item.getItemMeta()).setOwnerProfile(pProfile);
+
+        skullMeta.setOwnerProfile(pProfile);
+
+        item.setItemMeta(skullMeta);
         item.setAmount(amount);
+
         ItemMeta meta = item.getItemMeta();
         if (lore != "") {
             String[] loreListArray = lore.split("___");
@@ -91,6 +109,22 @@ public class Items {
             item.setItemMeta(headMeta);
         }
         return item;
+    }
+
+    private static PlayerProfile getProfile(String url) {
+
+        PlayerProfile profile = Bukkit.createPlayerProfile(UUID.nameUUIDFromBytes(("https://textures.minecraft.net/texture/"+url).getBytes())); // Get a new player profile
+        PlayerTextures textures = profile.getTextures();
+        URL urlObject;
+
+        try {
+            urlObject = new URL("https://textures.minecraft.net/texture/"+url);
+        } catch (MalformedURLException exception) {
+            throw new RuntimeException("Invalid URL : " + url);
+        }
+        textures.setSkin(urlObject); // Set the skin of the player profile to the URL
+        profile.setTextures(textures); // Set the textures back to the profile
+        return profile;
     }
 
 }
